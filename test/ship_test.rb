4566224b152a40da5bbb345190740aafd1817b7d
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-05-12 07:47:48 -0600

    Create and setup ship class and test files

---
+++ test/ship_test.rb
@@ -0,0 +1,14 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+
+class ShipTest < Minitest::Test
+
+  def setup
+    @cruiser = Ship.new("Cruiser", 3)
+  end
+
+  def test_it_exists
+    assert_instance_of Ship, @cruiser
+  end
+end
