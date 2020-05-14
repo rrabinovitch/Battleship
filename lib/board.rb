8b571bc1cfff150b64f23c5e64a7d9bd43d4225b
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-05-13 23:03:13 -0600

    Create and setup board class aand test files

---
+++ lib/board.rb
@@ -0,0 +1,7 @@
+class Board
+
+
+  def initialize(argument)
+    @argument = argument
+  end
+end
---
+++ test/board_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+
+class BoardTest < Minitest::Test
+  def setup
+
+  end
+
+  def test_it_exists
+
+  end
+end
