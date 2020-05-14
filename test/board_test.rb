78de6f1a9183f3303d2355b961f89421bc064248
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-05-13 23:33:16 -0600

    Add cells method and tests

--- test/board_test.rb
+++ test/board_test.rb
@@ -6,10 +6,12 @@ require './lib/board'
 
 class BoardTest < Minitest::Test
   def setup
-
+    @board = Board.new
   end
 
   def test_it_exists
-
+    assert_equal 16, @board.cells.count
+    assert_instance_of Hash, @board.cells
+    # a way to test that the keys within the cells method point to cell objects
   end
 end
