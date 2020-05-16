f5f5ba6c810107ccb3aa6a4f2d40ddfcc4d4f2f1
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-05-16 16:22:21 -0600

    Fix :bug: and add cell_has_no_ships? :sparkles:

--- lib/board.rb
+++ lib/board.rb
@@ -1,12 +1,13 @@
 class Board
-  attr_reader :ships
+  attr_reader :ships,
+              :cells
   def initialize
     @ships = []
-    @cells = {}
+    @cells = make_cells
   end
 
-  def cells
-    @cells = {
+  def make_cells
+    {
       "A1" => Cell.new("A1"), "A2" => Cell.new("A2"), "A3" => Cell.new("A3"), "A4" => Cell.new("A4"),
       "B1" => Cell.new("B1"), "B2" => Cell.new("B2"), "B3" => Cell.new("B3"), "B4" => Cell.new("B4"),
       "C1" => Cell.new("C1"), "C2" => Cell.new("C2"), "C3" => Cell.new("C3"), "C4" => Cell.new("C4"),
@@ -81,8 +82,14 @@ class Board
     end
   end
 
+  def cell_has_no_ships?(coordinates)
+    coordinates.all? do |coordinate|
+      @cells[coordinate].empty?
+    end
+  end
+
   def valid_placement?(ship, coordinates)
-    if coordinates_and_ship_length_same_num?(ship, coordinates) && coordinates_consecutive?(coordinates)
+    if coordinates_and_ship_length_same_num?(ship, coordinates) && coordinates_consecutive?(coordinates) && cell_has_no_ships?(coordinates) == true
       true
     else
       false
@@ -91,23 +98,25 @@ class Board
 
   def place(ship, coordinates)
     coordinates.each do |coordinate|
+      if @cells[coordinate].empty?
       @cells[coordinate].place_ship(ship)
+      end
     end
   end
 
-  def render(visible = false) #rendering the board. Is there an easier way to do this?
-    if visible == true
-      "  1 2 3 4 \n" +
-      "A #{@cells["A1"].render(true)} #{@cells["A2"].render(true)} #{@cells["A3"].render(true)} #{@cells["A4"].render(true)} \n" +
-      "B #{@cells["B1"].render(true)} #{@cells["B2"].render(true)} #{@cells["B3"].render(true)} #{@cells["B4"].render(true)} \n" +
-      "C #{@cells["C1"].render(true)} #{@cells["C2"].render(true)} #{@cells["C3"].render(true)} #{@cells["C4"].render(true)} \n" +
-      "D #{@cells["D1"].render(true)} #{@cells["D2"].render(true)} #{@cells["D3"].render(true)} #{@cells["D4"].render(true)} \n"
-    else
+  def render(visible = false) #Rendering the board. Is there an easier way to do this?
+    if visible == false
       "  1 2 3 4 \n" +
       "A #{@cells["A1"].render} #{@cells["A2"].render} #{@cells["A3"].render} #{@cells["A4"].render} \n" +
       "B #{@cells["B1"].render} #{@cells["B2"].render} #{@cells["B3"].render} #{@cells["B4"].render} \n" +
       "C #{@cells["C1"].render} #{@cells["C2"].render} #{@cells["C3"].render} #{@cells["C4"].render} \n" +
       "D #{@cells["D1"].render} #{@cells["D2"].render} #{@cells["D3"].render} #{@cells["D4"].render} \n"
+    else
+      "  1 2 3 4 \n" +
+      "A #{@cells["A1"].render(true)} #{@cells["A2"].render(true)} #{@cells["A3"].render(true)} #{@cells["A4"].render(true)} \n" +
+      "B #{@cells["B1"].render(true)} #{@cells["B2"].render(true)} #{@cells["B3"].render(true)} #{@cells["B4"].render(true)} \n" +
+      "C #{@cells["C1"].render(true)} #{@cells["C2"].render(true)} #{@cells["C3"].render(true)} #{@cells["C4"].render(true)} \n" +
+      "D #{@cells["D1"].render(true)} #{@cells["D2"].render(true)} #{@cells["D3"].render(true)} #{@cells["D4"].render(true)} \n"
     end
   end
 end
--- test/board_test.rb
+++ test/board_test.rb
@@ -112,56 +112,48 @@ class BoardTest < Minitest::Test
   end
 
   def test_ships_occupy_multiple_cells
-    @cell_1 = @board.cells["A1"]
-    @cell_2 = @board.cells["A2"]
-    @cell_3 = @board.cells["A3"]
-    #require "pry"; binding.pry
     @board.place(@cruiser, ["A1", "A2", "A3"])
+    cell_1 = @board.cells["A1"]
+    cell_2 = @board.cells["A2"]
+    cell_3 = @board.cells["A3"]
 
-
-    assert_equal @board.cells["A1"].ship, @board.cells["A2"].ship
-    assert_equal @board.cells["A2"].ship, @board.cells["A3"].ship
-
-
-    # assert_equal @cell_1.ship, @cruiser
-    # assert_equal @cell_2.ship, @cruiser
-    # assert_equal @cell_3.ship, @cruiser
-    # assert_equal @cell_2.ship, @cell_3.ship
+    assert_equal @cruiser, cell_1.ship
+    assert_equal @cruiser, cell_2.ship
+    assert_equal @cruiser, cell_3.ship
   end
 
-  #Was able to bypass the error above, but it is not resovled.
-  #However, it allowed me to finish the following tests.
-  #Will return back and fix these tests once we understand
-  #how to fix the error.
+  def test_if_all_cells_are_empty
+    @board.place(@cruiser, ["A1", "A2", "A3"])
+    assert_equal true, @board.cell_has_no_ships?(["B2", "B3", "B4"])
+    assert_equal false, @board.cell_has_no_ships?(["A1","A2","A3"])
+  end
 
   def test_ships_do_not_overlap
-    skip
     cell_1 = @board.cells["A1"]
     cell_2 = @board.cells["A2"]
     cell_3 = @board.cells["A3"]
     @board.place(@cruiser, ["A1", "A2", "A3"])
     @board.place(@submarine, ["A1", "A2"])
 
-    assert_equal cell_1.ship, @cruiser
-    assert_equal cell_2.ship, @cruiser
-    assert_equal cell_3.ship, @cruiser
+    assert_equal @cruiser, cell_1.ship
+    assert_equal @cruiser, cell_2.ship
+    assert_equal @cruiser, cell_3.ship
     assert_equal false, @board.valid_placement?(@submarine, ["A1", "B1"])
   end
-
-  def test_it_can_render_board
-    skip
-    expected = "  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n"
-
-    cell_1 = @board.cells["A1"]
-    cell_2 = @board.cells["A2"]
-    cell_3 = @board.cells["A3"]
-    cell_4 = @board.cells["A4"]
-    cell_5 = @board.cells["B4"]
-    @board.place(@submarine, ["A4", "B4"])
-    @board.place(@cruiser, ["A1", "A2", "A3"])
-    cell_1.fire_upon
-    cell_4.fire_upon
-    cell_5.fire_upon
-    assert_equal expected, @board.render(true)
-  end
 end
+
+  # def test_it_can_render_board
+  #   expected = "  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n"
+  #
+  #   cell_1 = @board.cells["A1"]
+  #   cell_2 = @board.cells["A2"]
+  #   cell_3 = @board.cells["A3"]
+  #   cell_4 = @board.cells["A4"]
+  #   cell_5 = @board.cells["B4"]
+  #   @board.place(@submarine, ["A4", "B4"])
+  #   @board.place(@cruiser, ["A1", "A2", "A3"])
+  #   cell_1.fire_upon
+  #   cell_4.fire_upon
+  #   cell_5.fire_upon
+  #   assert_equal expected, @board.render(true)
+  # end
