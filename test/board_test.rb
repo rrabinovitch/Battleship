edab09b891cff5ea1d5d8f593d3e130a5c34443e
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-05-16 01:29:39 -0600

    Complete placement validation testing methods

--- test/board_test.rb
+++ test/board_test.rb
@@ -27,16 +27,9 @@ class BoardTest < Minitest::Test
 
   def test_it_evaluates_congruence_between_ship_length_and_number_of_coordinates
     assert_equal false, @board.coordinates_and_ship_length_same_num?(@cruiser, ["A1", "A2"])
-    # assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2"])
-
     assert_equal false, @board.coordinates_and_ship_length_same_num?(@submarine, ["A2", "A3", "A4"])
-    # assert_equal false, @board.valid_placement?(@submarine, ["A2", "A3", "A4"])
-
     assert_equal true, @board.coordinates_and_ship_length_same_num?(@cruiser, ["A1", "A2", "A3"])
-    # assert_equal true, @board.valid_placement?(@cruiser, ["A1", "A2", "A3"])
-
     assert_equal true, @board.coordinates_and_ship_length_same_num?(@submarine, ["A2", "A3"])
-    # assert_equal true, @board.valid_placement?(@submarine, ["A2", "A3"])
   end
 
   def test_it_can_split_coordinates
@@ -87,14 +80,33 @@ class BoardTest < Minitest::Test
     assert_equal false, @board.coordinates_consecutive?(["C1", "B1"])
   end
 
-  def test_it_can_use_all_criteria_to_validate_placement
+  def test_coordinates_must_be_same_length_as_ship
+    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2"])
+    assert_equal false, @board.valid_placement?(@submarine, ["A2", "A3", "A4"])
+
+    assert_equal true, @board.valid_placement?(@cruiser, ["A1", "A2", "A3"])
+    assert_equal true, @board.valid_placement?(@submarine, ["A2", "A3"])
+  end
+
+  def test_coordinates_must_be_consecutive
     assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2", "A4"])
     assert_equal false, @board.valid_placement?(@submarine, ["A1", "C1"])
     assert_equal false, @board.valid_placement?(@cruiser, ["A3", "A2", "A1"])
     assert_equal false, @board.valid_placement?(@submarine, ["C1", "B1"])
 
-    #add assert_trues here too
+    assert_equal true, @board.valid_placement?(@cruiser, ["A1", "A2", "A3"])
+    assert_equal true, @board.valid_placement?(@cruiser, ["A1", "B1", "C1"])
+    assert_equal true, @board.valid_placement?(@submarine, ["C1", "D1"])
+    assert_equal true, @board.valid_placement?(@submarine, ["D1", "D2"])
   end
 
-  #make sure to test all helper methods
+  def test_coordinates_cant_be_diagonal
+    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "B2", "C3"])
+    assert_equal false, @board.valid_placement?(@submarine, ["C2", "D3"])
+  end
+
+  def test_it_can_use_all_criteria_to_validate_placement
+    assert_equal true, @board.valid_placement?(@submarine, ["A1", "A2"])
+    assert_equal true, @board.valid_placement?(@cruiser, ["B1", "C1", "D1"])
+  end
 end
